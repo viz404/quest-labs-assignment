61c666dbc8f69b6a22c7ae62b516443e46317cfe
vishal <vishal.gxm@gmail.com>
2023-04-21 14:14:20 +0530

    changed status bar color

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 
 import {StackNavigator} from "./navigators";
+import {StatusBar} from "react-native";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
       <StackNavigator />
     </NavigationContainer>
   );
